d26afa2ca7c822eddf3ef05b803bb3b636786491
Luis Gerardo <luisgpr99@gmail.com>
2020-01-16 23:34:34 -0600

    Updates

---
+++ precache-manifest.7e5cd4475dba1e1a5f625864c4364ffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61f6d073f63313cb23446b3a9b42184",
+    "url": "/PlanMed/index.html"
+  },
+  {
+    "revision": "8b0c3cb450a01bc06690",
+    "url": "/PlanMed/static/css/main.af52fd49.chunk.css"
+  },
+  {
+    "revision": "95a71a064b84e136fd01",
+    "url": "/PlanMed/static/js/2.0b57db54.chunk.js"
+  },
+  {
+    "revision": "444dc38bd626bde05d4fa219b64d3c28",
+    "url": "/PlanMed/static/js/2.0b57db54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8b0c3cb450a01bc06690",
+    "url": "/PlanMed/static/js/main.dade6089.chunk.js"
+  },
+  {
+    "revision": "456f71f280521b030b40",
+    "url": "/PlanMed/static/js/runtime-main.0cee4717.js"
+  }
+]);
